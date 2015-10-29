c5f5cceda61ffe5abb13d62f9db5f1b516dfc747
shiki49 <shiki49.com@gmail.com>
2015-10-30 08:04:16 +0900

    test json

--- go/test/test.go
+++ go/test/test.go
@@ -1,57 +1,25 @@
 package main
 
 import (
-	"bytes"
-	"encoding/gob"
+	"encoding/json"
 	"fmt"
-	"log"
 )
 
-type Data struct {
-	id   int32
-	name [16]byte
-}
-
-func (d *Data) GobEncode() ([]byte, error) {
-	w := new(bytes.Buffer)
-	encoder := gob.NewEncoder(w)
-	err := encoder.Encode(d.id)
-	if err != nil {
-		return nil, err
-	}
-	err = encoder.Encode(d.name)
-	if err != nil {
-		return nil, err
-	}
-	return w.Bytes(), nil
-}
-
-func (d *Data) GobDecode(buf []byte) error {
-	r := bytes.NewBuffer(buf)
-	decoder := gob.NewDecoder(r)
-	err := decoder.Decode(&d.id)
-	if err != nil {
-		return err
-	}
-	return decoder.Decode(&d.name)
+type User struct {
+	Name string
 }
 
 func main() {
-	d := Data{id: 7}
-	copy(d.name[:], []byte("tree"))
-	fmt.Println(d.name)
-	buffer := new(bytes.Buffer)
-	// writing
-	enc := gob.NewEncoder(buffer)
-	err := enc.Encode(d)
+	user1 := User{Name: "Frank"}
+	user2 := User{Name: "Bob"}
+
+	users := []User{}
+	users = append(users, user1)
+	users = append(users, user2)
+	b, err := json.Marshal(users)
 	if err != nil {
-		log.Fatal("encode error:", err)
+		fmt.Println(err)
+		return
 	}
-	// reading
-	buffer = bytes.NewBuffer(buffer.Bytes())
-	e := new(Data)
-	dec := gob.NewDecoder(buffer)
-	err = dec.Decode(e)
-	fmt.Println(e, err)
-	fmt.Println(e.name)
+	fmt.Println(string(b))
 }
