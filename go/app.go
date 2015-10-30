d9e0af44c3c31138dc82ead304e993f500c239c0
shiki49 <shiki49.com@gmail.com>
2015-10-30 09:20:04 +0900

    remove comment

--- go/app.go
+++ go/app.go
@@ -706,26 +706,6 @@ func GetFriends(w http.ResponseWriter, r *http.Request) {
 		friendsMap[id] = v
 	}
 
-	// rows, err := db.Query(`SELECT * FROM relations WHERE one = ? OR another = ? ORDER BY created_at DESC`, user.ID, user.ID)
-	// if err != sql.ErrNoRows {
-	// 	checkErr(err)
-	// }
-	// friendsMap := make(map[int]time.Time)
-	// for rows.Next() {
-	// 	var id, one, another int
-	// 	var createdAt time.Time
-	// 	checkErr(rows.Scan(&id, &one, &another, &createdAt))
-	// 	var friendID int
-	// 	if one == user.ID {
-	// 		friendID = another
-	// 	} else {
-	// 		friendID = one
-	// 	}
-	// 	if _, ok := friendsMap[friendID]; !ok {
-	// 		friendsMap[friendID] = createdAt
-	// 	}
-	// }
-	// rows.Close()
 	friends := make([]Friend, 0, len(friendsMap))
 	for key, val := range friendsMap {
 		friends = append(friends, Friend{key, val})
@@ -762,8 +742,6 @@ func PostFriends(w http.ResponseWriter, r *http.Request) {
 		}
 		conn.Do("SET", user.ID, string(stringfyJSON))
 
-		// _, err := db.Exec(`INSERT INTO relations (one, another) VALUES (?,?), (?,?)`, user.ID, another.ID, another.ID, user.ID)
-		// checkErr(err)
 		http.Redirect(w, r, "/friends", http.StatusSeeOther)
 	}
 }
